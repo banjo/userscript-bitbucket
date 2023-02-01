34c1d09e9e11c43dbd609d6a2b9473725452f130
Anton Ödman <anton.odman@gmail.com>
2023-02-01 10:09:01 +0100

    chore: publish latest build

--- dist/bitbucket-csp-mono-link.user.js
+++ dist/bitbucket-csp-mono-link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       bitbucket-csp-mono-link
 // @namespace  userscript/astra
-// @version    0.0.0
+// @version    0.0.1
 // @author     Anton
 // @match      https://bitbucket.org/*
 // @run-at     document-end
@@ -89,6 +89,95 @@
       clearInterval(runInterval);
     };
   };
+  const w = () => "undefined" != typeof window;
+  const Y = (e, n) => (n || document).querySelector(e);
+  Y.exists = (e, n) => n ? !!Y(e, n) : !!Y(e), Y.all = (e, n) => Array.from((n || document).querySelectorAll(e));
+  const icons = {
+    success: `<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 32 32"><path fill="currentColor" d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm-2 19.59l-5-5L10.59 15L14 18.41L21.41 11l1.596 1.586Z"/><path fill="none" d="m14 21.591l-5-5L10.591 15L14 18.409L21.41 11l1.595 1.585L14 21.591z"/></svg>`,
+    error: `<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 32 32"><path fill="currentColor" d="M16 2C8.2 2 2 8.2 2 16s6.2 14 14 14s14-6.2 14-14S23.8 2 16 2zm5.4 21L16 17.6L10.6 23L9 21.4l5.4-5.4L9 10.6L10.6 9l5.4 5.4L21.4 9l1.6 1.6l-5.4 5.4l5.4 5.4l-1.6 1.6z"/></svg>`,
+    warning: `<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 32 32"><path fill="currentColor" d="M16 2C8.3 2 2 8.3 2 16s6.3 14 14 14s14-6.3 14-14S23.7 2 16 2zm-1.1 6h2.2v11h-2.2V8zM16 25c-.8 0-1.5-.7-1.5-1.5S15.2 22 16 22s1.5.7 1.5 1.5S16.8 25 16 25z"/></svg>`
+  };
+  const backgroundColor = {
+    success: "#bbf7d0",
+    error: "#fecdd3",
+    warning: "#fff3cd"
+  };
+  const color = {
+    success: "#0f5132",
+    error: "#842029",
+    warning: "#664d03"
+  };
+  const defaultOptions = {
+    duration: 5e3,
+    type: "success",
+    animationTiming: 300,
+    fontSize: "1.2rem",
+    width: "fit-content",
+    useIcon: true
+  };
+  const toast = (message, options) => {
+    if (!w())
+      throw new Error("toast can only be used in a browser");
+    const { duration, type, animationTiming, fontSize, width, useIcon } = {
+      ...defaultOptions,
+      ...options
+    };
+    const toast2 = document.createElement("div");
+    toast2.classList.add("banjo-toast");
+    toast2.dataset.type = type;
+    toast2.dataset.duration = duration.toString();
+    toast2.style.cssText = `
+        transition: all ${animationTiming}ms ease;
+        font-size: ${fontSize};
+        width: ${width};
+        box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+        height: fit-content;
+        transform: translateX(200%);
+        position: fixed;
+        bottom: 2rem;
+        right: 2rem;
+        padding: 1rem;
+        border-radius: 0.5rem;
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        cursor: pointer;
+        z-index: 9999;
+        `;
+    toast2.style.backgroundColor = backgroundColor[type];
+    toast2.style.color = color[type];
+    if (useIcon) {
+      const icon = document.createElement("i");
+      icon.classList.add("banjo-toast-icon");
+      icon.innerHTML = icons[type];
+      toast2.appendChild(icon);
+    }
+    toast2.appendChild(document.createTextNode(message));
+    document.body.appendChild(toast2);
+    const show = () => {
+      toast2.style.transform = "translateX(0%)";
+    };
+    const hide = (element = toast2, beforeRemove = 5e3) => {
+      element.style.transform = "translateX(200%)";
+      setTimeout(() => {
+        var _a;
+        (_a = element.parentNode) == null ? void 0 : _a.removeChild(element);
+      }, beforeRemove);
+    };
+    toast2.addEventListener("click", () => {
+      hide();
+    });
+    if (Y.exists(".banjo-toast")) {
+      const oldToasts = Y.all(".banjo-toast");
+      oldToasts.forEach((oldToast) => {
+        const dur = oldToast.dataset.duration;
+        hide(oldToast, Number(dur) + animationTiming);
+      });
+    }
+    setTimeout(show, animationTiming);
+    setTimeout(hide, duration);
+    return { hide: () => hide() };
+  };
   const settingsButtonQuery = '[data-testid="settingsButton"]';
   window.onload = () => {
     run(main, {
@@ -128,8 +217,10 @@
     if (document.querySelector(".csp-button"))
       return;
     const settingsButton = document.querySelector(settingsButtonQuery);
-    if (!settingsButton)
+    if (!settingsButton) {
+      toast("Could not find settings button", { type: "error" });
       return;
+    }
     const buttonContainer = settingsButton.parentElement;
     const newContainer = buttonContainer == null ? void 0 : buttonContainer.cloneNode(true);
     const newButton = newContainer.querySelector("button");
@@ -141,15 +232,22 @@
     buttonContainer == null ? void 0 : buttonContainer.insertAdjacentElement("afterend", newContainer);
     newButton.addEventListener("click", async () => {
       const message = getBranch();
-      navigator.clipboard.writeText(formatMessage(message));
+      const size = prompt("Enter size", "S");
+      if (!size)
+        return;
+      navigator.clipboard.writeText(formatMessage(message, size));
+      toast(`Copied to clipboard in Markdown`, {
+        type: "success",
+        duration: 2e3
+      });
     });
   }
   function getBranch() {
-    var _a, _b;
-    const name = ((_b = (_a = document.querySelector("header form span a")) == null ? void 0 : _a.parentElement) == null ? void 0 : _b.textContent) ?? "";
+    var _a;
+    const name = ((_a = document.querySelector(".css-atqsw9 > span:nth-child(1)")) == null ? void 0 : _a.textContent) ?? "";
     return name;
   }
-  function formatMessage(branch) {
-    return `*S*, _csp-mono_: ${branch}`;
+  function formatMessage(branch, size) {
+    return `*${size}*, _csp-mono_: [${branch}](${window.location.href})`;
   }
 })();
