ad1ef35ec118e1ea6625bb36673d6f41fc7d3ee8
Anton Ödman <anton.odman@gmail.com>
2023-02-23 11:12:12 +0100

    chore: release v0.0.4

--- dist/bitbucket-csp-mono-link.user.js
+++ dist/bitbucket-csp-mono-link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       bitbucket-csp-mono-link
 // @namespace  userscript/astra
-// @version    0.0.3
+// @version    0.0.4
 // @author     Anton
 // @match      https://bitbucket.org/*
 // @run-at     document-end
